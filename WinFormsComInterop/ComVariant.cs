0dcf36b5f4731e358539bc274bacd807c4a642a1
William Kent <wjk011@gmail.com>
2021-07-09 15:55:06 -0400

    Add ComVariant helper struct

---
+++ WinFormsComInterop/ComVariant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace WinFormsComInterop
+{
+    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+    internal struct ComVariant : IDisposable
+    {
+        [DllImport("olaut32.dll")]
+        private static extern int VariantClear(IntPtr pVar);
+
+        public ComVariant(object value)
+        {
+            // This is the size of the largest field in a VARIANT, the decVal field.
+            // Since VARIANT is a union, all other data types will fit in this size.
+            Handle = Marshal.AllocCoTaskMem(sizeof(ushort) + sizeof(byte) * 2 + sizeof(uint) + sizeof(ulong));
+            if (value != null) Marshal.GetNativeVariantForObject(value, Handle);
+        }
+
+        public IntPtr Handle;
+
+        public object Value => Marshal.GetObjectForNativeVariant(Handle);
+
+        public void Dispose()
+        {
+            VariantClear(Handle);
+            Marshal.FreeCoTaskMem(Handle);
+        }
+    }
+}
