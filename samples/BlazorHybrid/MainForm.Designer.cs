21b62b576bb656216158bb613c18d4ec890fe00f
Andrii Kurdiumov <kant2002@gmail.com>
2022-12-23 19:39:36 +0600

    Make things work for BlazorHybrid

--- samples/BlazorHybrid/MainForm.Designer.cs
+++ samples/BlazorHybrid/MainForm.Designer.cs
@@ -36,7 +36,7 @@
             this.blazorWebView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.blazorWebView1.Location = new System.Drawing.Point(0, 0);
             this.blazorWebView1.Name = "blazorWebView1";
-            this.blazorWebView1.Size = new System.Drawing.Size(800, 450);
+            this.blazorWebView1.Size = new System.Drawing.Size(304, 151);
             this.blazorWebView1.TabIndex = 0;
             this.blazorWebView1.Text = "blazorWebView1";
             // 
@@ -44,11 +44,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(304, 151);
             this.Controls.Add(this.blazorWebView1);
             this.Name = "MainForm";
             this.Text = "Blazor + NativeAOT";
-            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
