3d7ac9e97878e3725e2c8156ac2f9356da15de23
Andrii Kurdiumov <kant2002@gmail.com>
2022-12-24 01:25:57 +0600

    NativeAOT does not support TypeIdentifier apparently and until resolution for https://github.com/dotnet/runtime/issues/79941 happens, I cannot do anything except supporting only latest WebView2

--- WinFormsComInterop/WebView2/ICoreWebView2Wrapper.cs
+++ WinFormsComInterop/WebView2/ICoreWebView2Wrapper.cs
@@ -5,8 +5,9 @@ using System.Runtime.InteropServices;
 namespace WinFormsComInterop.WebView2;
 
 [RuntimeCallableWrapper(typeof(webview2::Microsoft.Web.WebView2.Core.Raw.ICoreWebView2))]
-[RuntimeCallableWrapper(typeof(Microsoft.Web.WebView2.Core.Raw.ICoreWebView2Private))]
-[RuntimeCallableWrapper(typeof(Microsoft.Web.WebView2.Core.Raw.ICoreWebView2PrivatePartial))]
+//[RuntimeCallableWrapper(typeof(Microsoft.Web.WebView2.Core.Raw.ICoreWebView2Private))]
+//[RuntimeCallableWrapper(typeof(Microsoft.Web.WebView2.Core.Raw.ICoreWebView2PrivatePartial))]
+[RuntimeCallableWrapper(typeof(webview2::Microsoft.Web.WebView2.Core.Raw.ICoreWebView2PrivatePartial))]
 internal partial class ICoreWebView2Wrapper
 {
     public readonly IntPtr instance;
---
+++ facades/Microsoft.Web.WebView2.Core/Raw/ICoreWebView2PrivatePartial.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Web.WebView2.Core.Raw;
+
+[ComImport]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+[Guid("2C94DD56-E252-40A1-BA7E-B19417B26A60")]
+public interface ICoreWebView2PrivatePartial
+{
+    [MethodImpl(MethodImplOptions.InternalCall)]
+    void AddHostObjectHelper([In][MarshalAs(UnmanagedType.Interface)] ICoreWebView2PrivateHostObjectHelper helper);
+}
