48a2d44134b489f32dbd7021d6413269fe1b8da4
Andrii Kurdiumov <kant2002@gmail.com>
2023-01-06 17:03:47 +0600

    Small sample on Visual Basic

---
+++ SampleVisualBasic/MainForm.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ButtonAdd = New System.Windows.Forms.Button()
+        Me.ComboBoxSelection = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'ButtonAdd
+        '
+        Me.ButtonAdd.Location = New System.Drawing.Point(12, 12)
+        Me.ButtonAdd.Name = "ButtonAdd"
+        Me.ButtonAdd.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonAdd.TabIndex = 0
+        Me.ButtonAdd.Text = "Add"
+        Me.ButtonAdd.UseVisualStyleBackColor = True
+        '
+        'ComboBoxSelection
+        '
+        Me.ComboBoxSelection.FormattingEnabled = True
+        Me.ComboBoxSelection.Location = New System.Drawing.Point(93, 13)
+        Me.ComboBoxSelection.Name = "ComboBoxSelection"
+        Me.ComboBoxSelection.Size = New System.Drawing.Size(121, 23)
+        Me.ComboBoxSelection.TabIndex = 1
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComboBoxSelection)
+        Me.Controls.Add(Me.ButtonAdd)
+        Me.Name = "MainForm"
+        Me.Text = "MainForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ButtonAdd As Button
+    Friend WithEvents ComboBoxSelection As ComboBox
+End Class
